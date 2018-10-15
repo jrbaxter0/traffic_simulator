5182e9035188905400d732f144c6b6ec710d2218
Andrew <hegewald42@gmail.com>
2018-10-15 19:19:00 -0400

    Adding spawnpoint

---
+++ Traffic Light Example/Source Code/spawnpoint.js
@@ -0,0 +1,7 @@
+//This file defines the spawnpoint object
+//The attributes are x_dim, y_dim, direction, last_spawn, and spawn_freq
+
+//x_dim: x dimension on the car_grid
+//y_dim: y dimension on the car grid
+//direction: direction on the spawn grid
+//
