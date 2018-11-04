c1e55df3643644d3782aff839005023c300cc021
James Baxter <baxterjr@outlook.com>
2018-11-04 11:44:52 -0500

    added helper functions, comments, and cleaned up code

--- Traffic Light Example/Source Code/traffic_light.js
+++ Traffic Light Example/Source Code/traffic_light.js
@@ -1,14 +1,32 @@
-//This file defines the traffic_light object
-//The attributes are TODO
+// This file defines the traffic_light object
+// The attributes are map_row, map_col, cycle_mode, max_cars_horizontal, max_cars_vertical, max_time_horizontal_ms,
+// max_time_vertical_ms, last_green, state, state_time_ms
 
-//i_index: x dimension on the map
-//j_index: y dimension on the map
+// map_row					Row in map array
+// map_col					Column in map array
+// cycle_mode				Cycle mode "timer" or "count"
+// max_cars_horizontal		The maximum cars allowed to build up horizontally
+// max_cars_vertical		The maximum cars allowed to build up vertically
+// max_time_horizontal_ms	The maximum time spent green horizontally before changing
+// max_time_vertical_ms		The maximum time spent green vertically before changing
+// last_green 				The last green state, "horizontal" or "vertical"
+// state 					the state of the light, "red", "yellow", "vertical_green", "horizontal_green"
+// state_time_ms			the time that the light has spent in the state
 
-//last_state: the state of the light last time update_light was called
+// Constructor for timer based light
+function traffic_light_timer(map_row, map_col, max_time_horizontal_ms, max_time_vertical_ms)
+{
+	return traffic_light(map_row, map_col, "timer", 0, 0, max_time_horizontal_ms, max_time_vertical_ms);
+}
 
-//COMMENT
+// Constructor for count based light
+function traffic_light_count(map_row, map_col, max_cars_horizontal, max_cars_vertical)
+{
+	return traffic_light(map_row, map_col, "count", max_cars_horizontal, max_cars_vertical, 0, 0);
+}
 
-function traffic_light(i, j, cycle_mode, max_cars_horizontal, max_cars_vertical, max_time_horizontal_ms, max_time_vertical_ms)
+// General constructor
+function traffic_light(map_row, map_col, cycle_mode, max_cars_horizontal, max_cars_vertical, max_time_horizontal_ms, max_time_vertical_ms)
 {
 
 	/// @brief Time that the light is yellow before switching to double red
@@ -21,88 +39,109 @@ function traffic_light(i, j, cycle_mode, max_cars_horizontal, max_cars_vertical,
 	var light = {};
 
 	/// @brief Position of the intersection in the map array
-	light.index_i = i;
+	light.map_row = map_row;
 
 	/// @brief Position of the intersection in the map array
-	light.index_j = j;
+	light.map_col = map_col;
 
 	/// @brief The mode that the intersection will use - can be "car_count" or "timer"
 	light.cycle_mode = cycle_mode;
 
+	/// @brief The max cars in horizontal and vertical directions
 	light.max_cars_horizontal = max_cars_horizontal;
 	light.max_cars_vertical = max_cars_vertical;
 
+	/// @brief The max time in horizontal and vertical modes
 	light.max_time_horizontal_ms = max_time_horizontal_ms;
 	light.max_time_vertical_ms = max_time_vertical_ms;
 
-	light.last_green = "vertical"; //should this be set to something else?
+	// Initial state setup
+	light.last_green = "vertical";
 	light.state = "red";
 	light.state_time_ms = -light_time_red_ms; // Will make it turn green on first cycle
 	
 
-	light.update_light = function(car_grid){
-		switch(this.state){
+	light.update_light = function(car_grid)
+	{
+		switch(this.state)
+		{
 			case "red": 
-				if(this.state_time_ms > light_time_red_ms){
-					if(this.last_green == "horizontal"){
+				if(this.state_time_ms > light_time_red_ms)
+				{
+					if(this.last_green == "horizontal")
+					{
 						this.state = "vertical_green";
 					}
-					else{
+					else
+					{
 						this.state = "horizontal_green";
 					}
 					this.state_time_ms = 0;
 				}
-				else{
+				else
+				{
 					this.state_time_ms += 1; // can we get real ms?
 				}
 				break;
+
 			case "yellow": 
 				if(this.state_time_ms > light_time_yellow_ms)
 				{
 					this.state = "red";
 					this.state_time_ms = 0;
 				}
+				else
+				{
+					this.state_time_ms += 1; // can we get real ms?
+				}
 				break;
-			case "vertical_green" // Intentional overflow
+
 			case "horizontal_green"
-				if(this.cycle_mode == "timer"){
-					if(this.state == "horizontal_green"){
-						if(this.state_time_ms > this.max_time_horizontal_ms){
-							this.state = "yellow";
-							this.state_time_ms = 0;
-							this.last_green = "horizontal";
-						}
-						else{
-							this.state_time_ms += 1;
-						}
-					}
-					else //Vertical green
+				if(this.cycle_mode == "timer")
+				{
+					if(this.state_time_ms > this.max_time_horizontal_ms)
 					{
-						if(this.state_time_ms > this.max_time_vertical_ms){
-							this.state = "yellow";
-							this.state_time_ms = 0;
-							this.last_green = "vertical";
-						}
-						else{
-							this.state_time_ms += 1;
-						} 
+						this.state = "yellow";
+						this.state_time_ms = 0;
+						this.last_green = "horizontal";
+					}
+					else
+					{
+						this.state_time_ms += 1;
 					}
 				}
-				else{ //Timer mode
-					if(this.state == "horizontal_green"){
-						if(this.get_cars(car_grid, "vertical") > this.max_cars_vertical){
-							this.state = "yellow";
-							this.state_time_ms = 0;
-							this.last_green = "horizontal";
-						}
-					}
-					else //Vertical green
+				else //count
+				{
+					if(this.get_cars(car_grid, "vertical") > this.max_cars_vertical)
 					{
-						if(this.get_cars(car_grid, "horizontal") > this.max_cars_horizontal){
-							this.state = "yellow";
-							this.state_time_ms = 0;
-							this.last_green = "vertical";
-						} 
+						this.state = "yellow";
+						this.state_time_ms = 0;
+						this.last_green = "horizontal";
+					}
+				}
+				break;
+
+			case "vertical_green"
+				if(this.cycle_mode == "timer")
+				{
+					if(this.state_time_ms > this.max_time_vertical_ms)
+					{
+						this.state = "yellow";
+						this.state_time_ms = 0;
+						this.last_green = "vertical";
+					}
+					else
+					{
+						this.state_time_ms += 1;
+					}
+				}
+				else //count
+				{
+					if(this.get_cars(car_grid, "horizontal") > this.max_cars_horizontal)
+					{
+						this.state = "yellow";
+						this.state_time_ms = 0;
+						this.last_green = "vertical";
 					}
 				}
 				break;
